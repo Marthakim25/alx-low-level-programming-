e6d0bfbf614ffb2229086434163380f3689d20c7
Marthakim25 <umukoromarthaoghenetega70@gmail.com>
2022-10-13 15:30:22 -0700

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main- Servers as the entry for the program
+ *
+ * Returns: By default returns zero if no errors otherwise returns non-zero value.
+ */
+int main(void)
+{
+ puts(*\programming is like building a multilingual puzzle");
+ return (0);
+} 
