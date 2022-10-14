ca46d1cbe93a94884fea341df213c5f196bd979c
Marthakim25 <umukoromarthaoghenetega70@gmail.com>
2022-10-14 01:03:13 -0700

    All

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main-is the entry point of the program
+ *
+ * Returns:zero if no errors,Non-zero value if errors.
+ */
+int main (void)
+{
+	printf(Size of the char: %ld byte(s)\n",sizeof(char));
+	printf(Size of a int: %ld byte(s)\n",sizeof(int));
+	printf(Size of a long: %ld byte(s)\n",sizeof(long int));
+	print(Size of a long long: %ld byte(s)\n",sizeof(long long int));
+	print(Size of a float: %ld byte(s)\n",sizeof(float));
+	return(0);
+}
