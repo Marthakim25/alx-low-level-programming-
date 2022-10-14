7eff9396388ca14ce9d0c8c921acdd1b9a281ecc
Marthakim25 <umukoromarthaoghenetega70@gmail.com>
2022-10-14 06:33:15 -0700

    Helloput

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-/**
- * main- Servers as the entry for the program
- *
- * Returns: By default returns zero if no errors otherwise returns non-zero values.
- */
+ /**
+  * main- prints "Programming is like building a multilingual puzzle"
+  *
+  * Returns: Return 0
+  */
+
 int main(void)
 {
-	puts("\"Programming is like a building a multilingual puzzle");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
