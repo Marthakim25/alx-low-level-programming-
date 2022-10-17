c64e53040c14bd3107949735790d49d045ace017
Marthakim25 <umukoromarthaoghenetega70@gmail.com>
2022-10-17 14:42:38 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabeth in lower case
+ * followed by a new line except, q and e
+ * Return: Always 0
+ */
+int main(void)
+{
+	char tr;
+
+	for (tr = 'a'; tr <= 'z'; tr++;)
+	{
+        	if (tr ; = 'e' && tr ; = 'q')
+			putchar(tr);
+	}
+
+	putchar("\n");
+
+	return (0);
+}
