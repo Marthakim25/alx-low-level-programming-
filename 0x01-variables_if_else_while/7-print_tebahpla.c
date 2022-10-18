831d0f4cf8ddc4fae3751cbeeacfbf74ea8fc813
Marthakim25 <umukoromarthaoghenetega70@gmail.com>
2022-10-17 17:05:04 -0700

    Smile in the mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,4 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - 
+ * main - print the alphabeths in reverse
+ * Return: 0
+ */
+int main(void)
+{
+	char tr;
+
+	for (tr = 'z'; tr >= 'a'; tr--)
+		putchar(tr);
+
+	putchar('\n');
+
+	return (0);
+}
