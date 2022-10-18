4aa9a28fdaa13e23ff7dd426d8e57c1c3ab94e5f
Marthakim25 <umukoromarthaoghenetega70@gmail.com>
2022-10-18 16:23:50 -0700

    insomnia 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alpha in lowercase
+ */
+void print_alphabet(void)
+{
+	char tr;
+
+	for (tr = 'a'; tr <= 'z'; tr++)
+		_putchar(tr);
+
+	_putchar('\n');
+}
