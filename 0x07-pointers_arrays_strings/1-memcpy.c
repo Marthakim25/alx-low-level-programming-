de87de460bb69096ddb5ac658e48c14ed18b26d8
Marthakim25 <umukoromarthaoghenetega70@gmail.com>
2022-11-01 04:14:31 -0700

    Heart

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,11 +12,11 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	char *ptr = dest;
 
-	while (n-- < 0)
+	while (n-- > 0)
 	{
 		*dest = *src;
-		*dest++;
-		*src++;
+		dest++;
+		src++;
 	}
 
 	return (ptr);
