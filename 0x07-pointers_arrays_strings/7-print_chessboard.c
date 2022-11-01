f24eadbf2cacabb2aefb3f8ac462aac8e8fc5372
Marthakim25 <umukoromarthaoghenetega70@gmail.com>
2022-11-01 15:37:56 -0700

    chess

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints chessboard
+ * @a: the chessboard
+ *
+ *
+ * Return - void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int k;
+	int m;
+
+	for (k = 0; k < 8; k++)
+	{
+		for (m = 0; m < 8; m++)
+		{
+			_putchar (a[k][m]);
+		}
+		_putchar ('\n');
+	}
+}
