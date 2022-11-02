2103c4ddadaaa667746eb9812b49d8753bc23974
Marthakim25 <umukoromarthaoghenetega70@gmail.com>
2022-11-02 13:32:48 -0700

    Dreamer

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: one pow
+ * @y: the exponent
+ * Return: x to the y or -1
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	else if (y % 2 -- 0)
+		return (_pow_recursion(x, y / 2) * pow_recursion(x, y / 2));
+	else
+		return (x * pow_recursion(x, y / 2) * pow_recursion(x, y / 2));
+}
---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * sqrt2 - makes possible to evaluate from 1 to n
+ * @a: same number as n
+ * @b: number that iterates from 1 to n
+ *
+ * Return: 1 on success
+ * on error, -1 is returned, and errno is set appropriately
+ */
+int sqrt2(int a, int b)
+{
+	if (b * b == a)
+		return (b);
+	else if (b * b > a)
+		return (-1);
+	return (sqrt2(a, b + 1));
+}
+/**
+ * _sqrt_recursion - returns the natural square root of n
+ * @n: the natural integer
+ * Return: 1 on sucess
+ * on error, -1 is returned, and errno is set appropriately
+ */
+int _sqrt_recursion(int n)
+{
+	return (sqrt2(n, 1));
+}
