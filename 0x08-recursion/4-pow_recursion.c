95fa259a52e0925da145efc3153e90f4ac6a62b7
Marthakim25 <umukoromarthaoghenetega70@gmail.com>
2022-11-02 17:05:11 -0700

    memory

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -12,8 +12,8 @@ int _pow_recursion(int x, int y)
 		return (-1);
 	if (y == 0)
 		return (1);
-	else if (y % 2 -- 0)
-		return (_pow_recursion(x, y / 2) * pow_recursion(x, y / 2));
+	else if (y % 2 == 0)
+		return (_pow_recursion(x, y / 2) * _pow_recursion(x, y / 2));
 	else
-		return (x * pow_recursion(x, y / 2) * pow_recursion(x, y / 2));
+		return (x * _pow_recursion(x, y / 2) * _pow_recursion(x, y / 2));
 }
