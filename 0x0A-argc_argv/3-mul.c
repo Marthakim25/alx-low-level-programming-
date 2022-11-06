e8197a1dca674edb72df8007654daf742dce417b
Marthakim25 <umukoromarthaoghenetega70@gmail.com>
2022-11-06 07:16:35 -0800

    multipy with three

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,16 +9,16 @@
  */
 int main(int argc, char *argv[])
 {
-	int result, num2, num3;
+	int result, num1, num2;
 
-	if (argc != 4)
+	if (argc != 3)
 	{
 		printf("%s\n", "Error");
 		return (1);
 	}
+	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
-	num3 = atoi(argv[3]);
-	result = num2 * num3;
+	result = num1 * num2;
 
 	printf("%d\n", result);
 	return (0);
