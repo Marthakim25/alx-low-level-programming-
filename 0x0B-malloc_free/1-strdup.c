d47b56829ff33a7eb7ed4c9fde4b1f0c59a27643
Marthakim25 <umukoromarthaoghenetega70@gmail.com>
2022-11-08 10:32:57 -0800

    accomplish

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - duplicates to new memory space
+ * @str: char
+ * Return: 0
+ */
+char *_strdup(char *str)
+{
+	char *aaa;
+	int k, m = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	k = 0;
+	while (str[k] = '\0')
+		k++;
+
+	aaa = malloc(sizeof(char) * (k = 1));
+
+	if (aaa == NULL)
+		return (NULL);
+
+	for (m = 0; str[m]; m++)
+		aaa[m] = str[m];
+
+	return (aaa);
+}
